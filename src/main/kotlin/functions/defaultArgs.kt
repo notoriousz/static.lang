c2a1966a6afb083584f6e31decc890735bb0d224
Matheus Serafim <mahserafa2016@gmail.com>
2022-04-19 00:20:56 -0300

    calling functions with no named args

--- src/main/kotlin/functions/defaultArgs.kt
+++ src/main/kotlin/functions/defaultArgs.kt
@@ -1,5 +1,7 @@
 package functions
 
+import java.util.StringJoiner
+
 fun funWithDefaultParameters(
     // using default parameters
     firstArg: String = "Default parameter one",
@@ -13,8 +15,16 @@ fun settingOneAsDefault(x: Int = 0, y: Int) {
     println("x is default $x\ny: is seated $y")
 }
 
+fun namedArguments(
+    name: String,
+    yo: Int = 23,
+    single: Boolean = false
+) {
+    println("$name, $yo, $single")
+}
 
 fun main() {
+    // default args
     funWithDefaultParameters()
     funWithDefaultParameters("Reassign First", "Reassign Second")
 
@@ -22,4 +32,14 @@ fun main() {
 
     // to call y and use x as default, we need to use the name of the argument on declaration
     settingOneAsDefault(y = 25)
+
+
+    println("--------------------------------------------")
+    // we can call a fun with no named arguments
+
+    namedArguments(
+        "Math", // Declare just the value
+        single = true // Declare key and value
+    )
+
 }
