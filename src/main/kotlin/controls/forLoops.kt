854a5317b9bef685285582aaf18a0c199e4334a5
Matheus Serafim <mahserafa2016@gmail.com>
2022-04-20 00:27:59 -0300

    add controls structures 02

--- src/main/kotlin/controls/forLoops.kt
+++ src/main/kotlin/controls/forLoops.kt
@@ -1,2 +1,33 @@
 package controls
 
+fun forLoops() {
+    val myList = arrayOf<Int>(1, 2, 3, 4, 5)
+
+    // basic
+    for (item: Int in myList) {
+        println("block: $item")
+    }
+
+    // expression
+    for (item: Int in myList) println("expression: $item")
+
+    // range
+    for (i in 1..3) {
+        println(i)
+    }
+
+    // range with steps and regression
+    for (i in 6 downTo 0 step 2) {
+        println(i)
+    }
+
+    // for with the index and value as destructuring
+    for ((index, value) in myList.withIndex()) {
+        println("the element at $index is $value")
+    }
+
+}
+
+fun main() {
+    forLoops()
+}
