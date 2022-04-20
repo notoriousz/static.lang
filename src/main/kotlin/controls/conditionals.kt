a0153df5aa8e7a7b6cc84863e50707073929add6
Matheus Serafim <mahserafa2016@gmail.com>
2022-04-20 00:20:22 -0300

    add controls structure samples

---
+++ src/main/kotlin/controls/conditionals.kt
@@ -0,0 +1,40 @@
+package controls
+
+
+const val a: Int = 2
+const val b: Int = 1
+
+fun ifExpression() {
+    // if with block else
+    if (a > b) {
+        println(a)
+    } else {
+        println(b)
+    }
+
+    // if as expression
+    val maxValue = if (a > b) a else b
+    println(maxValue)
+
+
+    // when expression
+
+    when (a) {
+        1 -> println("a == 1")
+        2 -> println("a == 2")
+        else -> {
+            println("No one")
+        }
+    }
+
+    when (b) {
+        in -10..-1 -> println("b is negative")
+        in 1..10 -> println("b is positive")
+    }
+}
+
+
+
+fun main() {
+    ifExpression()
+}
