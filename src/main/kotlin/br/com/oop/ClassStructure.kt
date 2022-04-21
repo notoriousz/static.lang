b955f57bfea6ad160c160fa5957df6322259347e
Matheus Serafim <mahserafa2016@gmail.com>
2022-04-21 18:27:45 -0300

    adding samples to basic collections

--- src/main/kotlin/br/com/oop/ClassStructure.kt
+++ src/main/kotlin/br/com/oop/ClassStructure.kt
@@ -1,4 +1,4 @@
-package classes
+package br.com.oop
 
 class ClassStructure {
 }
--- src/main/kotlin/br/com/serafim/DatasStructures.kt
+++ src/main/kotlin/br/com/serafim/DatasStructures.kt
@@ -1,11 +1,26 @@
 package br.com.serafim
 
+import java.util.StringJoiner
+
 fun main() {
     // creating a array -> in array we cannot add and remove itens
     val array = arrayOf("Matheus", "Carlos", "Fabio")
-
+    println(array.joinToString())
 
     // creating a list -> in list we can add and remove, is more dinamic
-    val list = listOf<String>("Giovanna", "Leia", "Vanessa")
+    val list = mutableListOf<String>("Giovanna", "Leia", "Vanessa")
+    list.add(0, "João")
+    println(list.joinToString())
 
+    // creating sets -> the elements doesn't repeat
+    val set = setOf<String>("Vigesimal", "Pedro", "Carla")
+    println(set.joinToString())
+
+
+    // creating maps
+    val map = mutableMapOf(
+        "Name" to "Matheus",
+        "ID" to 1
+    )
+    println(map.toString())
 }
