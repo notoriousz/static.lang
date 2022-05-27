573a0998e986ad1f7094d62d87f417063278b0f6
Matheus Serafim <mahserafa2016@gmail.com>
2022-05-27 18:19:46 -0300

    Init block

--- src/main/kotlin/br/com/object/InitBlock.kt
+++ src/main/kotlin/br/com/object/InitBlock.kt
@@ -1,4 +1,9 @@
 package br.com.`object`
 
-class InitBlock {
+class InitBlock(name: String) {
+    // another form to use constructor, but isn't useful
+    val myName: String
+    init {
+        this.myName = name
+    }
 }
