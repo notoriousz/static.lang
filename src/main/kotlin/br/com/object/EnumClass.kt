bfec12b5f17dbd08a41d6ef73e6f7faa35ed6d52
Matheus Serafim <mahserafa2016@gmail.com>
2022-05-27 19:25:12 -0300

    ENUM CLASS

---
+++ src/main/kotlin/br/com/object/EnumClass.kt
@@ -0,0 +1,9 @@
+package br.com.`object`
+
+enum class EnumClass {
+    DOMINGO, SEGUNDA, TERCA, QUARTA, QUINTA, SEXTA, SABADO
+}
+
+fun main() {
+    println("Test: ${EnumClass.DOMINGO} ${EnumClass.QUINTA   }")
+}
